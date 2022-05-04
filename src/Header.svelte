44f67aa19da6ad50791f879acebbdab4506b0c61
Arshdeep Singh <arshdeepsingh.singh7@gmail.com>
2022-05-04 23:51:48 +0530

    Add files via upload

---
+++ src/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1> </h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <!-- <img class="image" src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" />  -->
+      <span>Welcome <strong>{$username}</strong></span>
+      
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+
+    {:else}
+
+      <h3> </h3>
+  {/if}
+</header>
---
+++ src/Login.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { user } from './user';
+
+  let username;
+  let password;
+
+  function login() {
+    user.auth(username, password, ({ err }) => err && alert(err));
+  }
+
+  function signup() {
+    user.create(username, password, ({ err }) => {
+      if (err) {
+        alert(err);
+      } else {
+        login();
+      }
+    });
+  }
+</script>
+<center><img class="logo" src='./assets/logo.png' alt="Logo">
+<!-- <label for="username">Username</label> -->
+<input name="username" placeholder="Username" bind:value={username} minlength="3" maxlength="16" />
+<br><br>
+<!-- <label for="password">Password</label> -->
+<input name="password" placeholder="Password" bind:value={password} type="password" />
+<br><br>
+<button class="login" on:click={login}>Login</button>
+
+<h2 class="signup" on:click="{signup}"> New to Privv? Sign Up</h2>
+</center>
+<!-- <button class="login"  on:click={signup}>Sign Up</button> -->
+
+<h2 class="footer"> Truly Indian 🇮🇳 </h2>
+<h3 class="developed"> Developed by Arshdeep & Dhruval </h3>
